a1862cb18626a047caf207309df242b1e2be8186
krishnakumarjha1893 <krishnakumarjha1893@gmail.com>
2026-09-02 23:52:10 +0530

    feat(model): define base fields in Vehicle.java

--- src/com/quickride/model/vehicle/Vehicle.java
+++ src/com/quickride/model/vehicle/Vehicle.java
@@ -1,7 +1,15 @@
 package com.quickride.model.vehicle;
 public class Vehicle{
- public static void main(String[] args){
-System.out.println("Hello mic testing....");
+ 
+private String userId;
+
+private String fullName;
+
+private String phoneNumber;
+
+private double walletBalance;
+
+
+
  }
 
-}
